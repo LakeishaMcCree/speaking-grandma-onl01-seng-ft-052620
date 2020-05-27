f8c9302a2719f91917652c67da9a66047780c4e1
Lakeisha McCree <lmccree215@icloud.com>
2020-05-27 16:13:31 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -14,7 +14,8 @@
 def speak_to_grandma(string)
   if string == "I LOVE YOU GRANDMA!" 
    "I LOVE YOU TOO PUMPKIN!"
-  elsif string == string.upcase
+  elsif string != string.upcase
+  
   end
   
   
